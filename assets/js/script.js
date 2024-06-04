0e15b1b79a901f8c932c728e2fd83e1f147e7054
leenabaxioned <leenab@axioned.com>
2024-06-04 19:52:16 +0530

    script added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,27 @@
 /* Author: */
+var circles = document.querySelectorAll(".circle");
+var btn = document.querySelectorAll("button");
+var progressBar = document.querySelector(".progress-bar");
+
+let count = 0;
+btn.forEach((element) => {
+  element.addEventListener("click", function () {
+    if (element.id === "next") {
+      if (count < 3) {
+        count++;
+        circles[count].classList.add("active");
+        element.classList.add("activebtn")
+      }else {
+        element.classList.remove("activebtn")
+      }
+    } else if (element.id === "prev") {
+      if (count > 0) {
+        count--;
+        circles[count + 1].classList.remove("active");
+        element.classList.add("activebtn")
+      }else{
+        element.classList.remove("activebtn")
+      }
+    }
+  });
+});
